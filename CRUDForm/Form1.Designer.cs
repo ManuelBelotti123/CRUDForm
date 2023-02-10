25465d32c45c82a9a6b52fab9d21e8c2d85004d6
Belotti.20181 <Belotti.20181@LABI2-13PC3>
2023-02-10 13:56:01 +0100

    Creazione paraziale della funzione di ricerca

--- CRUDForm/Form1.Designer.cs
+++ CRUDForm/Form1.Designer.cs
@@ -31,40 +31,74 @@
             this.Input = new System.Windows.Forms.TextBox();
             this.liststampa = new System.Windows.Forms.ListView();
             this.AggButton = new System.Windows.Forms.Button();
+            this.StampaArr = new System.Windows.Forms.Button();
+            this.CancElem = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Input
             // 
-            this.Input.Location = new System.Drawing.Point(30, 48);
+            this.Input.Location = new System.Drawing.Point(30, 56);
             this.Input.Name = "Input";
-            this.Input.Size = new System.Drawing.Size(103, 20);
+            this.Input.Size = new System.Drawing.Size(215, 20);
             this.Input.TabIndex = 1;
             // 
             // liststampa
             // 
             this.liststampa.HideSelection = false;
-            this.liststampa.Location = new System.Drawing.Point(449, 12);
+            this.liststampa.Location = new System.Drawing.Point(465, 27);
             this.liststampa.Name = "liststampa";
-            this.liststampa.Size = new System.Drawing.Size(339, 426);
+            this.liststampa.Size = new System.Drawing.Size(304, 382);
             this.liststampa.TabIndex = 2;
             this.liststampa.UseCompatibleStateImageBehavior = false;
             this.liststampa.View = System.Windows.Forms.View.List;
             // 
             // AggButton
             // 
-            this.AggButton.Location = new System.Drawing.Point(30, 103);
+            this.AggButton.Location = new System.Drawing.Point(30, 91);
             this.AggButton.Name = "AggButton";
-            this.AggButton.Size = new System.Drawing.Size(133, 23);
+            this.AggButton.Size = new System.Drawing.Size(215, 23);
             this.AggButton.TabIndex = 3;
             this.AggButton.Text = "Aggiungi un elemento";
             this.AggButton.UseVisualStyleBackColor = true;
             this.AggButton.Click += new System.EventHandler(this.AggButton_Click);
+            // 
+            // StampaArr
+            // 
+            this.StampaArr.Location = new System.Drawing.Point(30, 183);
+            this.StampaArr.Name = "StampaArr";
+            this.StampaArr.Size = new System.Drawing.Size(215, 23);
+            this.StampaArr.TabIndex = 4;
+            this.StampaArr.Text = "Stampa dell\'array";
+            this.StampaArr.UseVisualStyleBackColor = true;
+            // 
+            // CancElem
+            // 
+            this.CancElem.Location = new System.Drawing.Point(30, 120);
+            this.CancElem.Name = "CancElem";
+            this.CancElem.Size = new System.Drawing.Size(215, 23);
+            this.CancElem.TabIndex = 5;
+            this.CancElem.Text = "Cancella un elemento";
+            this.CancElem.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Carlito", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 33);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Gestione di un array";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CancElem);
+            this.Controls.Add(this.StampaArr);
             this.Controls.Add(this.AggButton);
             this.Controls.Add(this.liststampa);
             this.Controls.Add(this.Input);
@@ -80,6 +114,9 @@
         private System.Windows.Forms.TextBox Input;
         private System.Windows.Forms.ListView liststampa;
         private System.Windows.Forms.Button AggButton;
+        private System.Windows.Forms.Button StampaArr;
+        private System.Windows.Forms.Button CancElem;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- CRUDForm/Form1.cs
+++ CRUDForm/Form1.cs
@@ -35,18 +35,40 @@ namespace CRUDForm
         private void AggButton_Click(object sender, EventArgs e)
         {
             //funzione che aggiunge un elemento
-            aggiunta(array, ref dim, Input.Text);
+            Aggiunta(array, ref dim, Input.Text);
             //aggiorno la ListView
             liststampa.Items.Add(Input.Text);
         }
 
         //funzioni servizio
-        public void aggiunta(string[] array, ref int dim, string elemento)
+        public void Aggiunta(string[] array, ref int dim, string elemento)
         {
+            //trasformo in minuscolo la stringa
+            elemento = elemento.ToLower();
             //inserisco l'elemento nella prima posizione disponibile dell'array
             array[dim] = elemento;
             //incremento la dimensione
             dim++;
+        }
+
+        public int Ricerca(string[] array, string elemricercato, ref int dim)
+        {
+            int pos = 0;
+            //trasformo in minuscolo la stringa
+            elemricercato = elemricercato.ToLower();
+            //ciclo di ricerca
+            for (int i = 0; i < dim; i++)
+            {
+                if (array[i] == elemricercato)
+                {
+                    pos = i;
+                }
+                else
+                {
+                    return pos = -1;
+                }
+            }
+            return pos;
         }
     }
 }
