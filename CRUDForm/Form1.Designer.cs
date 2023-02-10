527300618da539c1821fbb311eb47b7f63e716d0
Belotti.20181 <Belotti.20181@LABI2-13PC3>
2023-02-10 13:26:31 +0100

    Creazione della funzione di aggiunta

--- CRUDForm/Form1.Designer.cs
+++ CRUDForm/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.liststampa = new System.Windows.Forms.ListView();
+            this.AggButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(30, 48);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(103, 20);
+            this.Input.TabIndex = 1;
+            // 
+            // liststampa
+            // 
+            this.liststampa.HideSelection = false;
+            this.liststampa.Location = new System.Drawing.Point(449, 12);
+            this.liststampa.Name = "liststampa";
+            this.liststampa.Size = new System.Drawing.Size(339, 426);
+            this.liststampa.TabIndex = 2;
+            this.liststampa.UseCompatibleStateImageBehavior = false;
+            this.liststampa.View = System.Windows.Forms.View.List;
+            // 
+            // AggButton
+            // 
+            this.AggButton.Location = new System.Drawing.Point(30, 103);
+            this.AggButton.Name = "AggButton";
+            this.AggButton.Size = new System.Drawing.Size(133, 23);
+            this.AggButton.TabIndex = 3;
+            this.AggButton.Text = "Aggiungi un elemento";
+            this.AggButton.UseVisualStyleBackColor = true;
+            this.AggButton.Click += new System.EventHandler(this.AggButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AggButton);
+            this.Controls.Add(this.liststampa);
+            this.Controls.Add(this.Input);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.ListView liststampa;
+        private System.Windows.Forms.Button AggButton;
     }
 }
 
--- CRUDForm/Form1.cs
+++ CRUDForm/Form1.cs
@@ -12,9 +12,41 @@ namespace CRUDForm
 {
     public partial class Form1 : Form
     {
+        //dichiarazioni
+        public int dim;
+        public string[] array;
+
+        //funzioni evento
+        //costruttore della classe
         public Form1()
         {
             InitializeComponent();
+            //inizializzazioni e dimensionamenti
+            array = new string[100];
+            dim = 0;
+        }
+
+        //carica il form
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AggButton_Click(object sender, EventArgs e)
+        {
+            //funzione che aggiunge un elemento
+            aggiunta(array, ref dim, Input.Text);
+            //aggiorno la ListView
+            liststampa.Items.Add(Input.Text);
+        }
+
+        //funzioni servizio
+        public void aggiunta(string[] array, ref int dim, string elemento)
+        {
+            //inserisco l'elemento nella prima posizione disponibile dell'array
+            array[dim] = elemento;
+            //incremento la dimensione
+            dim++;
         }
     }
 }
